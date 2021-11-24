1bb38f08fa8581ddbfde8ae372fab6a89967027d
Rickie Li <rickie.li.x@gmail.com>
2021-11-24 16:34:59 -0500

    did some code

--- electron.py
+++ electron.py
@@ -2,14 +2,17 @@ import numpy as np
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
-# Function to calculate electromagnetic field value
-def electromag_func():
-    return None
+# Coil electromagnetic field expressed as a function of 1/r where x is 1/r
+def field_coil_r_func(x, a, b):
+    return x*a - b
 
+#Function to calculate coil electromagnetic field
+def field_coil_func(x):
+    return ((4/5)**(3/2))*4*np.pi*(10**(-7))*130*x/0.155
 
 # Function for error calculation
-def electromag_error_func():
-    return None
+def field_coil_error_func(a, b, c):
+    return a*np.sqrt(((c/b)**2)+((0.0025/0.155)**2))
 
 
 # Model function for constant voltage
@@ -46,4 +49,19 @@ if __name__ == "__main__":
     current_const_rad = data_current[:,4]
     current_const_rad_error = data_current[:5]
 
+    #data for the coil field at constant voltage
+    field_coil = field_coil_func(voltage_const_currents)
+    field_coil_error = field_coil*np.sqrt(((voltage_const_currents_error
+                                            /voltage_const_currents)**2)+
+                                          ((0.0025/0.155)**2))
     
+    #Curve_fit on field_coil_r_func for external field, b
+    popt, pcov = curve_fit(field_coil_r_func, 1/voltage_const_rad,
+                           field_coil, sigma=field_coil_error, 
+                           absolute_sigma=True)
+    
+    #model data
+    field_coil_r = field_coil_r_func(1/voltage_const_rad, popt[0], popt[1])
+    
+    #plot
+    plt.plot(1/voltage_const_rad, field_coil_r)
